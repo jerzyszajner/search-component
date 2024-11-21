b787baf1a78847ac9bf943143bedb89991560b9e
jerzy szajner <jerzy.szajner@gmail.com>
2024-11-21 15:19:10 +0100

    init commit

---
+++ src/js/app.js
@@ -0,0 +1,37 @@
+// const car = {
+//   brand: "Mercedes",
+//   color: "white",
+//   transmission: "automatic",
+//   year: 2022,
+//   maxSpeed: 260,
+// };
+
+// const myCar = ({ brand, color, year, transmission }) => {
+//   console.log(
+//     `My car is ${color} ${brand} from ${year} and It has an ${transmission} transmission .`
+//   );
+// };
+
+// myCar(car);
+
+
+// const filterdArray = [];
+// const removeDuplicate = (numbers) => {
+//   numbers.forEach((number) => {
+//     if (!filterdArray.includes(number)) {
+//       filterdArray.push(number);
+//     }
+//   });
+// };
+
+// removeDuplicate([2, 3, 6, 6, 9]);
+
+// console.log(filterdArray);
+
+
+
+const primeNumber = () => {
+    for(i = 0;  ) 
+}
+
+primeNumber(15);
